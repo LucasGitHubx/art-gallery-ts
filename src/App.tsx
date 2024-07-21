83ad26ba0bad3cf7c1080df9077c89519ba54bb6
LucasGitHubx <lucasdarosa450@gmail.com>
2024-07-21 20:05:08 -0300

    I added a new route to the app.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,14 @@ import {
 } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
 import HomePage from "./pages/HomePage";
+import AddPhotoPage from "./pages/AddPhotoPage";
 
 export default function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/art-gallery" element={<MainLayout />}>
         <Route index element={<HomePage />} />
+        <Route path="/art-gallery/add" element={<AddPhotoPage />} />
       </Route>
     )
   );
