3a28814c2fe5e3e40241d46238323da151470303
LucasGitHubx <lucasdarosa450@gmail.com>
2024-07-22 13:23:13 -0300

    I added a new functionality into de firestore.ts file. I allows you to get all the photos from the database.

--- src/firebase/firestore.ts
+++ src/firebase/firestore.ts
@@ -6,6 +6,7 @@ import {
   getDocs,
   getFirestore,
   addDoc,
+  query,
 } from "firebase/firestore";
 
 const db = getFirestore(app);
@@ -23,4 +24,26 @@ export async function addPhoto(
   } catch (error: any) {
     alert(error.code);
   }
+}
+
+export async function getPhotos(
+  setData: (photos: Photo[]) => void,
+  setLoaded: (val: boolean) => void
+) {
+  const photos: Photo[] = [];
+  const q = query(photosCollection);
+  const data = await getDocs(q);
+
+  data.docs.forEach((doc) => {
+    photos.push({
+      photoID: doc.id,
+      title: doc.data().title,
+      author: doc.data().author,
+      url: doc.data().url,
+      description: doc.data().description,
+    });
+  });
+
+  setData(photos);
+  setLoaded(true);
 }
