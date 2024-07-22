a5f840a66e3477ca1d06f690285ae1ceba784606
LucasGitHubx <lucasdarosa450@gmail.com>
2024-07-22 14:03:41 -0300

    Adding all the functionality on the gallery component. It will show all the photos that are stored in the database.

--- src/pages/GalleryPage.tsx
+++ src/pages/GalleryPage.tsx
@@ -1,10 +1,38 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./galleryPage.css";
 import { Photo } from "../types";
+
+// Database Actions
+import { getPhotos } from "../firebase/firestore";
+import PhotoRender from "../components/PhotoRender";
 
 export default function GalleryPage() {
   const [data, setData] = useState<Photo[]>([]);
   const [loaded, setLoaded] = useState<boolean>(false);
 
-  return;
+  useEffect(() => {
+    getPhotos(setData, setLoaded);
+  }, []);
+
+  return (
+    <div className="gallery-page container">
+      <p>
+        Welcome to the gallery, here you'll be able to see what other users have
+        uploaded. Please, if you think that a photo breaks the rules, contact
+        us.
+      </p>
+
+      {!loaded ? (
+        <h2>Loading...</h2>
+      ) : data.length === 0 ? (
+        <h2>There are no photos for the moment</h2>
+      ) : (
+        <div className="gallery">
+          {data.map((photo) => {
+            return <PhotoRender photo={photo} key={photo.photoID} />;
+          })}
+        </div>
+      )}
+    </div>
+  );
 }
